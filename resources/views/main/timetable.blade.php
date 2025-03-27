78b4be4ddbad71d29bbe1dfb0e510124dd338a68
Yaroslav Kolisnuk <kolisnuka2005@gmail.com>
2025-03-27 15:02:37 +0200

    tablets header logo

--- resources/views/main/timetable.blade.php
+++ resources/views/main/timetable.blade.php
@@ -1056,10 +1056,7 @@
         </div>
     </section>
 
-    <div class="snackbars" id="form-output-global"></div>
-    <script src="{{ asset('js/core.min.js') }}"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
-
+   
 </x-app-layout>
 
 
