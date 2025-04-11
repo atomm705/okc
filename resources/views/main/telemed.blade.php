51eddd786b4fafd0d065ac6dbd94e01e52c4af56
Yaroslav Kolisnuk <kolisnuka2005@gmail.com>
2025-04-11 10:33:50 +0300

    page telemed

--- resources/views/main/telemed.blade.php
+++ resources/views/main/telemed.blade.php
@@ -1,80 +1,194 @@
 <x-app-layout>
-    <section class="breadcrumbs-custom bg-image context-dark slider-page" style="background-image: url({{ asset('images/bg-6.webp') }});"  data-preset='{"title":"Breadcrumbs","category":"header","reload":false,"id":"breadcrumbs"}'>
+
+    <section class="breadcrumbs-custom bg-image context-dark slider-page"
+             style="background-image: url({{ asset('images/bg-6.webp') }});"
+             data-preset='{"title":"Breadcrumbs","category":"header","reload":false,"id":"breadcrumbs"}'>
         <div class="container">
             <h2 class="breadcrumbs-custom-title">
                 Online medical information</h2>
             <ul class="breadcrumbs-custom-path">
                 <li><a href="index.html">Home</a></li>
                 <li class="active">
-                    Online medical information</li>
+                    Online medical information
+                </li>
             </ul>
         </div>
     </section>
 
-    <section class="section-lg bg-default section">
-    <div class="container">
-        <h3 class="text-center">@lang('messages.Online medical information in Docart')</h3>
-        <div class="offset-top-41">
-            <p class="custom-paragraph">@lang('messages.The Medical Center "Dokart" is constantly improving the level of comfort of its clients. Now we offer the opportunity to consult with any specialist online.')</p>
-
-        </div>
-    </div>
-    </section>
 
 
 
+        <section class="section-lg bg-default-liac"><!----> <!---->
+            <div class="container text-center"><!----> <!---->
 
-    <section class="section-lg bg-default section">
-        <div class="container">
-            <h3 class="text-center">@lang('messages.Online medical information in Docart')</h3>
-            <div class="offset-top-41">
-                <p class="custom-paragraph">Це дистанційна онлайн бесіда з лікарем медичного центру. Під час неї спеціаліст вислухає ваші скарги (збере анамнез), проаналізує результати лабораторних досліджень, скорегує раніше призначене ним лікування, відповість на запитання, які вас цікавлять. В цілому медичне онлайн інформування нічим не відрізняється від контрольного відвідування лікаря.</p>
+                    <div ><h3 style="font-size: 2.60em; margin-top: 0px";>Медичне онлайн інформування в Докарт</h3>
+                        <p class="custom-paragraph">Медичний центр «Докарт» постійно підвищує рівень комфорту своїх клієнтів. Зараз ми пропонуємо
+                            можливість проконсультуватися з будь-яким спеціалістом онлайн.</p>
+                    </div>
 
             </div>
-        </div>
-    </section>
 
-
-
-    <section class="section-lg bg-default section">
-        <div class="container">
-            <h3 class="text-center">У яких випадках можна отримати медичне онлайн інформування?</h3>
-            <div class="offset-top-41">
-                <p class="custom-paragraph">Повністю погоджуємося з тим, що не можна замінити візити до лікарні дистанційними консультаціями. Багато процедур і обстежень виконуються виключно під час особистого прийому. Та все ж таки онлайн спілкування можливе у випадках :</p>
-
-
-                <div class="col-md-10 col-lg-8">
-                    <h6>Unordered and Ordered Lists</h6>
-                    <hr class="text-subline">
-                    <p>Our professional staff provides the following services:</p>
-                    <ul class="list list-marked">
-                        <li>Best Equipment</li>
-                        <li>Ultrasound Diagnostics</li>
-                        <li>X-ray Diagnostics</li>
-                        <li>Pediatrics</li>
-                    </ul>
-                    <p>Our professional staff provides the following services:</p>
-                    <ol class="list list-numbered" type="1">
-                        <li><span>Best Equipment</span></li>
-                        <li><span>Ultrasound Diagnostics</span></li>
-                        <li><span>X-ray Diagnostics</span></li>
-                        <li><span>Pediatrics</span></li>
-                    </ol>
-                </div>
+            <div class="div-telemed-link"><a href="https://uatelemd.com/okcentre/"
+                                 class="btn btn-ellipse btn-primary ">Записатися</a>
             </div>
-        </div>
-    </section>
+        </section>
 
+        <section class="section-lg bg-default-liac"><!----> <!---->
+            <div class="container text-center"><!----> <!---->
 
-    <section class="section-lg bg-default section">
-        <div class="container">
-            <h3 class="text-center">@lang('messages.Online medical information in Docart')</h3>
-            <div class="offset-top-41">
-                <p class="custom-paragraph">@lang('messages.The Medical Center "Dokart" is constantly improving the level of comfort of its clients. Now we offer the opportunity to consult with any specialist online.')</p>
+                    <div ><h3 style="font-size: 2.60em; margin-top: 0px";>Що таке медичне онлайн інформування у лікаря?</h3>
+                        <p class="custom-paragraph">Це дистанційна онлайн бесіда з лікарем медичного центру. Під час неї спеціаліст
+                            вислухає ваші скарги (збере анамнез), проаналізує результати лабораторних досліджень, скорегує
+                            раніше призначене ним лікування, відповість на запитання, які вас цікавлять. В цілому медичне
+                            онлайн інформування нічим не відрізняється від контрольного відвідування лікаря.</p>
+                    </div>
 
             </div>
+
+
+        </section>
+
+        <section class="section-lg bg-default-liac"><!----> <!---->
+        <div class="container text-center"><!----> <!---->
+
+            <div ><h3 style="font-size: 2.60em; margin-top: 0px";>У яких випадках можна отримати медичне онлайн інформування?</h3>
+                <p>Повністю погоджуємося з тим, що не можна замінити візити до лікарні дистанційними
+                    консультаціями. Багато процедур і обстежень виконуються виключно під час особистого прийому. Та
+                    все ж таки онлайн спілкування можливе у випадках :</p>
+
+                <ul class="list-marked" style="text-align: left;">
+                    <li>необхідності отримання альтернативної думки лікаря (у вас є результати аналізів та висновок
+                        іншого спеціаліста і ви хочете швидко перевірити коректність встановленого діагнозу та
+                        призначеного лікування);
+                    </li>
+                    <li>планового візиту, який не передбачає пальпації, прослуховування, УЗД та інших маніпуляцій;
+                    </li>
+                    <li>корекції призначеного раніше лікування;</li>
+                    <li>розшифровки аналізів;</li>
+                    <li>призначення додаткових медикаментозних препаратів у межах започаткованого раніше курсу;</li>
+                    <li>необхідності консультації сімейного лікаря;</li>
+                    <li>виникнення скарг і запитань, пов'язаних з лікуванням або загальним станом здоров'я;</li>
+                    <li>передачі інформації про ваші показники (артеріальний тиск, рівень цукру, вага та інші
+                        вимірювання, які можна провести вдома).
+                    </li>
+                </ul>
+            </div>
+
+            <div class="div-telemed-link"><a href="https://uatelemd.com/okcentre/"
+                                             class="btn btn-ellipse btn-primary ">Записатися</a>
+            </div>
         </div>
+
+        </section>
+
+
+    <section class="section-lg bg-default-liac"><!----> <!---->
+        <div class="container "><!----> <!---->
+
+            <div ><h3 style="font-size: 2.60em; margin-top: 0px"; class="text-center">Як проходить процедура дистанційної консультації у лікаря?</h3>
+                <p class="custom-paragraph text-center">Якщо коротко, то послідовність дій наступна :</p>
+
+                <ul class="list-marked" style="text-align: left;">
+                    <li>1. Ви залишаєте заявку на сайті.</li>
+                    <li>2. З вами зв'язується наш менеджер.</li>
+                    <li>3. Ви обираєте зручний час.</li>
+                    <li>4. Проводите оплату.</li>
+                    <li>5. У призначені день та час спеціаліст проводить консультацію.</li>
+                </ul>
+
+                <p class="text-left">Онлайн прийом проходить на порталі uatelemed.com. Усе, що вам
+                    необхідно, це – стабільний інтернет та вебкамера на пристрої. Якщо бесіда передбачає огляд,
+                    наприклад, трихолога або дерматолога, в кімнаті має бути хороше освітлення.</p>
+
+                <p >Як користуватися сервісом на сайті UaTelemed?</p>
+
+                <ul class="list-marked" style="text-align: left;">
+                    <li>1. На нашому сайті натискаєте кнопку «Онлайн інформування».</li>
+                    <li>2. Заповнюєте всі поля у формі заявки.</li>
+                    <li>3. За необхідності прикріплюєте файли.</li>
+                    <li>4. Натискаєте «Далі».</li>
+                    <li>5. Відзначаєте галочками, що ви погоджуєтеся з умовами використання сервісу.</li>
+                    <li>6. Підтверджуєте запит кліком на кнопці «Запит підтверджую».</li>
+                    <li>7. З вами зв'язується менеджер по телефону.</li>
+                    <li>8. На пошту вам приходить лист з посиланням на підтвердження.</li>
+                    <li>9. Ви переходите за посиланням та оплачуєте послугу.</li>
+                    <li>10. За 5-10 хвилин до початку консультації ви заходите в особистий кабінет сервісу
+                        uatelemed.com.
+                    </li>
+                    <li>11. У призначений час з вами зв'язується лікар.</li>
+                </ul>
+
+                <p>Тривалість консультації 20 - 30 хвилин. Рекомендуємо заздалегідь
+                    підготувати перелік запитань. Звертаємо увагу, якщо мета консультації – отримання альтернативної
+                    думки спеціаліста й у вас є відскановані результати аналізів, то більш зручно і швидше переслати
+                    їх лікарю під час відправлення заявки, щоб він ознайомився з документами до зустрічі з вами.</p>
+            </div>
+
+            <div class="div-telemed-link"><a href="https://uatelemd.com/okcentre/"
+                                             class="btn btn-ellipse btn-primary ">Записатися</a>
+            </div>
+        </div>
+
     </section>
+
+
+    <section class="section-lg bg-default-liac"><!----> <!---->
+        <div class="container "><!----> <!---->
+
+            <div ><h3 style="font-size: 2.60em; margin-top: 0px;" class="text-center">З яких питань можна записатися на медичне онлайн інформування до лікаря?</h3>
+                <p >Список скарг залежить від сфери діяльності
+                    спеціаліста. Медичне онлайн інформування у трихолога й дерматолога проводиться з питань
+                    випадіння і пересадки волосся, дерматозів, дерматитів. А ось проблемні родимки вимагають більш
+                    пильної уваги. Перед їх видаленням необхідно провести ряд аналізів і подивитися на них
+                    «вживу».</p>
+
+
+                <p class="text-left">Ендокринологи «Докарт» надають медичне онлайн інформування пацієнтів
+                    з цукровим діабетом, хронічними патологіями щитовидної залози, скаргами на зміну ваги,
+                    набряклість, порушення менструального циклу.</p>
+
+                <p >Гастроентеролог медичне онлайн інформуванн тільки тих, кому вже було
+                    призначено лікування.</p>
+                <p >Щоб дізнатися, чи зможуть вам допомогти віддалено, зателефонуйте нам
+                    за допомогою одного з телефонів:</p>
+
+                <ul class="list-marked" style="text-align: left;">
+                    <li>(0800) 317-317</li>
+                    <li>(057) 761-61-31</li>
+                    <li>(093) 761-61-31</li>
+                    <li>(099) 548-44-85</li>
+                </ul>
+
+
+            </div>
+
+
+        </div>
+
+    </section>
+    
+    <section class="section-lg bg-default-liac"><!----> <!---->
+        <div class="container text-center"><!----> <!---->
+
+            <div ><h3 style="font-size: 2.60em; margin-top: 0px";>Плюси онлайн інформування</h3>
+                <p>Складно не відзначити переваги дистанційного прийому:</p>
+
+                <ul class="list-marked" style="text-align: left;">
+                    <li>1. Консультації проводить професійний лікар.</li>
+                    <li>2. Не потрібно виходити з дому.</li>
+                    <li>3. Ви економите час і гроші на дорогу.</li>
+                    <li>4. Не потрібно сидіти в чергах і перетинатися з іншими хворими.</li>
+                </ul>
+            </div>
+
+            <div class="div-telemed-link"><a href="https://uatelemd.com/okcentre/"
+                                             class="btn btn-ellipse btn-primary ">Записатися</a>
+            </div>
+        </div>
+
+    </section>
+
+
 
 
 </x-app-layout>
